472a504c282b22cc147575b6f963667c950802a9
alexrcq <67456592+alexrcq@users.noreply.github.com>
2023-06-24 09:19:30 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 git config --global --add safe.directory /github/workspace
 hub checkout ${REPO_BRANCH:-master}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*.apk -m ${VERSION_NAME} $(date +%Y%m%d%H%M%S)
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*.apk -m "${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
